a7d74418025a50975d34e311208977fdd3159649
Stefan Zimmermann <zimmermann.code@gmail.com>
2014-10-10 20:51:39 +0000

    mIEEE488_2

---
+++ modeled/visa/ieee488_2.py
@@ -0,0 +1,27 @@
+# python-modeled.visa
+#
+# Copyright (C) 2014 Stefan Zimmermann <zimmermann.code@gmail.com>
+#
+# python-modeled.visa is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# python-modeled.visa is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with python-modeled.visa.  If not, see <http://www.gnu.org/licenses/>.
+
+"""modeled.visa.ieee488_2
+
+.. moduleauthor:: Stefan Zimmermann <zimmermann.code@gmail.com>
+"""
+from . import mInstrument, mcmd
+
+
+class IEEE488_2(mInstrument):
+    ident = mcmd.property[str]('*IDN?')
+    calibrate = mcmd[int]('*CAL?')
